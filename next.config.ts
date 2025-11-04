fbca6fcea9789daaca8b1f479fbfd3498acc5612
Vasilis <billyfili321@gmail.com>
2025-11-04 23:06:27 +0200

    chore: ignore ESLint during builds

--- next.config.ts
+++ next.config.ts
@@ -2,15 +2,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  eslint: {
-    // Don't fail the Vercel build on ESLint issues
-    ignoreDuringBuilds: true,
-  },
+  eslint: { ignoreDuringBuilds: true },
   images: {
-    // Optional: allow Unsplash etc. if you switch to next/image later
-    remotePatterns: [
-      { protocol: "https", hostname: "images.unsplash.com" },
-    ],
+    remotePatterns: [{ protocol: "https", hostname: "images.unsplash.com" }],
   },
 };
 
